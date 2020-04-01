d0bf27d6bd33cab463468c4747a27f8afc308809
evh671 <eyalnahom@gmail.com>
2020-04-01 16:37:36 +0300

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_GHTXTYyxneQnyfTV3oA44faY00vrvTQ4tM";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.png"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
+
+/*** STRIPE FAKE CREDIT CARD DETAILS ***/
